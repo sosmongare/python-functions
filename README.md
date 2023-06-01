8602a3553f3b8a93d473f1f67f7c73bfcf8c4c36
Sospeter Mongare <sosmongare@gmail.com>
2023-06-01 06:10:32 +0300

    Add readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+#Sum of two numbers
+
+Here, add_numbers(5, 4) specifies that arguments num1 and num2 will get values 5 and 4 respectively.
---
+++ sum-of-two-numbers.py
@@ -0,0 +1,9 @@
+# function with two arguments
+def add_numbers(num1, num2):
+    sum = num1 + num2
+    print("Sum: ",sum)
+
+# function call with two values
+add_numbers(5, 4)
+
+# Output: Sum: 9
